6ad11c72de8a82a8cbd6cc2209a130e7f7c548c5
Juluur <xxnikolaevxx@gmail.com>
2025-03-23 22:47:35 +0900

    добавил функцию sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -7,3 +7,12 @@ def filter_by_state(transactions: list[dict], state: str = 'EXECUTED') -> list[d
     :return: Новый список словарей, где 'state' соответствует заданному значению.
     """
     return [transaction for transaction in transactions if transaction.get('state') == state]
+def sort_by_date(transactions: list[dict], reverse: bool = True) -> list[dict]:
+    """
+    Сортирует список словарей по дате.
+
+    :param transactions: Список словарей для сортировки.
+    :param reverse: Если True, сортировка по убыванию (по умолчанию). Если False, по возрастанию.
+    :return: Новый список словарей, отсортированный по дате.
+    """
+    return sorted(transactions, key=lambda x: x['date'], reverse=reverse)
