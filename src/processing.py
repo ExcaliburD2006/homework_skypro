4a300910dc157aecc8a81ee1bdec13fa956179bc
Juluur <xxnikolaevxx@gmail.com>
2025-03-24 21:47:09 +0900

    переделал ошибки

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(transactions: list[dict], state: str = 'EXECUTED') -> list[dict]:
+def filter_by_state(transactions: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
     Фильтрует список словарей по значению ключа 'state'.
 
@@ -6,13 +6,16 @@ def filter_by_state(transactions: list[dict], state: str = 'EXECUTED') -> list[d
     :param state: Значение ключа 'state' для фильтрации (по умолчанию 'EXECUTED').
     :return: Новый список словарей, где 'state' соответствует заданному значению.
     """
-    return [transaction for transaction in transactions if transaction.get('state') == state]
+    return [transaction for transaction in transactions if transaction.get("state") == state]
+
+
 def sort_by_date(transactions: list[dict], reverse: bool = True) -> list[dict]:
     """
     Сортирует список словарей по дате.
 
     :param transactions: Список словарей для сортировки.
-    :param reverse: Если True, сортировка по убыванию (по умолчанию). Если False, по возрастанию.
+    :param reverse: Если True, сортировка по убыванию (по умолчанию)
+    Если False, по возрастанию.
     :return: Новый список словарей, отсортированный по дате.
     """
-    return sorted(transactions, key=lambda x: x['date'], reverse=reverse)
+    return sorted(transactions, key=lambda x: x["date"], reverse=reverse)
